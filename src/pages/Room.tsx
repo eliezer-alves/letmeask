ffa45ceae51c655d53d154067f9fa51859150ed1
eliezer <eliezer.c.alves2015@gmail.com>
2022-02-07 16:57:05 -0300

    feat(rooms): estilizando a pagina de exibir a sala

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,6 +1,32 @@
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+import '../styles/room.scss';
 
 export function Room() {
-    return (
-        <h1>Salas</h1>
-    );
+	return (
+		<div id="page-room">
+			<header>
+				<div className="content">
+					<img src={logoImg} alt="Letmeask" />
+					<div>código</div>
+				</div>
+			</header>
+
+			<main className="content">
+				<div className="room-title">
+					<h1>Sala React</h1>
+					<span>4 perguntas</span>
+				</div>
+
+				<form>
+					<textarea placeholder="O que você quer pergunytar?" />
+					<div className="form-footer">
+						<span>Para enviar uma pergunta, <button>faça seu login</button></span>
+						<Button type="submit">Enviar Pergunta</Button>
+					</div>
+				</form>
+			</main>
+
+		</div>
+	);
 }
