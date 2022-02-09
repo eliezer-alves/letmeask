59ca33bdb09c71ad85d400337cdcdd2ed9c954e0
eliezer <eliezer.c.alves2015@gmail.com>
2022-02-08 21:53:57 -0300

    feat(questions): criando component Question para a listagem das perguntas

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
+import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
@@ -125,6 +126,19 @@ export function Room() {
 						<Button type="submit" disabled={!user}>Enviar Pergunta</Button>
 					</div>
 				</form>
+				
+				<div className="question-list">
+
+					{questions.map(question => {
+						return (
+							<Question
+								key={question.id}
+								content={question.content}
+								author={question.author}
+							/>
+							);
+					})}
+				</div>
 			</main>
 		</div>
 	);
