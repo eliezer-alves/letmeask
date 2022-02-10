323bc2f3e536110fc651db3e327d4284aa2239a4
eliezer <eliezer.c.alves2015@gmail.com>
2022-02-10 13:40:12 -0300

    feat(questions): removendo opcao de dar like para perguntas ja respondidas

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -78,8 +78,8 @@ export function AdminRoom() {
 								isAnswered={question.isAnswered}
 								isHighlighted={question.isHighlighted}
 							>
-								{/* {!question.isAnswered && (
-									<> */}
+								{!question.isAnswered && (
+									<>
 										<button
 											className={question.isAnswered ? 'active-icon' : ''}
 											onClick={() => { handleCheckQuestionAsAnswered(question.id, question.isAnswered)} }
@@ -98,8 +98,8 @@ export function AdminRoom() {
 												<path fill-rule="evenodd" clip-rule="evenodd" d="M12 17.9999H18C19.657 17.9999 21 16.6569 21 14.9999V6.99988C21 5.34288 19.657 3.99988 18 3.99988H6C4.343 3.99988 3 5.34288 3 6.99988V14.9999C3 16.6569 4.343 17.9999 6 17.9999H7.5V20.9999L12 17.9999Z" stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
 											</svg>
 										</button>
-									{/* </>
-								)} */}
+									</>
+								)}
 
 								<button onClick={() => { handleDeleteQuestion(question.id)} }>
 									<img src={deleteImg} alt="Remover pergunta" />
